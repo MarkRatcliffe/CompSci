27e3caa09c8bce7e59492e20d74fef0873f3f783
MarkRatcliffe <36260703+MarkRatcliffe@users.noreply.github.com>
2019-11-15 12:46:57 +0000

    Initial commit
    Initial commit as I move the repository

---
+++ 13. SerialSearch/Searching/Searching/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Searching
+{
+    public partial class Form1 : Form
+    {
+        private Searching mySearching = new Searching();
+
+        private String[] theListToSearch;
+        private int numOfItems = 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+            theListToSearch = new String[numOfItems];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            resultsLabel1.Text = " ";
+            resultsLabel2.Text = " ";
+            int result = mySearching.SerialSearch(theListToSearch, searchText.Text);
+            if (result == -1)
+            {
+                resultsLabel1.Text = "String not found";
+            }
+            else
+            {
+                resultsLabel1.Text = "String found at: ";
+                resultsLabel2.Text = result.ToString();
+            }
+
+        }
+
+        private void stringList_TextChanged(object sender, EventArgs e)
+        {
+            int lineCount = 0;
+            foreach (string line in stringList.Lines) // pull out the lines from the textbox
+            {
+                //working with 'line' - one at a time
+                if (line != null)
+                {
+                    theListToSearch[lineCount] = line;
+                    lineCount++;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ 13. SerialSearch/Searching/Searching/Searching.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    class Searching
+    {
+        public int SerialSearch(String[] theList, String itemToSearchFor)
+        {
+            // check if the array is empty, if so return an error
+
+
+            // Use a for loop, initialise counter = 0 (first position/index in array)  
+            // set the number of iterations the same as the last position/index of the array 
+            // (so to that each location in the array can be checked)
+
+
+            // check if item (at current position) is equal to item searched
+            // if item found return the position/index (the value of the counter)
+
+            return -1; // placeholder so it compiles :-)
+        }
+
+        public int binarySearch(String[] theList, String itemToSearchFor)
+        {
+            return 1;
+        }
+
+    }
+}
