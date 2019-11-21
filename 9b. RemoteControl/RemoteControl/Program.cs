a48691daf4211a634b74ac9c64a7e121b8cba1be
Mark Ratcliffe <mbr@hereford.ac.uk>
2019-11-21 09:08:10 +0000

    Added in example code

--- 9b. RemoteControl/RemoteControl/Program.cs
+++ 9b. RemoteControl/RemoteControl/Program.cs
@@ -6,7 +6,50 @@ namespace RemoteControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void ProgramRemote()
+            {
+                Console.WriteLine("This is where I will code Program Remote");
+
+            }
+            void DisplayRemote()
+            {
+                Console.WriteLine("This is where I will display settings for Program Remote");
+
+            }
+
+            void runRemote()
+            {
+                Console.WriteLine("This is where I will code the running of Program Remote");
+
+            }
+            int option = 4;
+            do
+            {
+                Console.WriteLine("Welcome to remote control");
+                Console.WriteLine("Which option do you require?");
+                Console.WriteLine("1. Program your remote control");
+                Console.WriteLine("2. Display commands");
+                Console.WriteLine("3. Run");
+                Console.WriteLine("4. Quit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        ProgramRemote();
+                        break;
+                    case 2:
+                        DisplayRemote();
+                        break;
+                    case 3:
+                        runRemote();
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for using the Remote Control");
+                        break;
+                }
+            } while (option != 4);
+            Console.WriteLine("Bye");
+            Console.ReadLine(); // keep screen visible to allow user to read then die
         }
     }
 }
--- GitPatchVS.ps1
+++ GitPatchVS.ps1
@@ -2,7 +2,7 @@ $mode = Read-Host -Prompt "[S]et or [R]emove proxy?"
 $git = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd"
 if ($mode -eq "S")
 {
-    $pass = Read-Host -AsSecureString -Prompt "Please enter your password"
+    $pass = Read-Host -AsSecureString -Prompt "Please enter your HSFC password"
     & $git\git.exe config --global http.proxy http://${env:Username}:${pass}@172.20.10.10:8080
     & $git\git.exe config --global https.proxy https://${env:Username}:${pass}@172.20.10.10:8080
     & $git\git.exe config --global http.sslBackend "openssl"
