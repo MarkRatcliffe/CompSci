c178dfb17ca420395c61d55fc866463027eb70c1
mbr@hereford.ac.uk <mbr@hereford.ac.uk>
2019-12-02 09:29:30 +0000

    Initialisation

---
+++ 15a. PoemWordCounter/PoemWordCounter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PoemWordCounter
+{
+    class Program
+    {
+        WordCounter dictionary = new WordCounter(100);
+
+        static void Main(string[] args)
+        {
+             // read in line of text
+             // process line of text, looking for individual words
+                   // for each word for WordCounter AddString
+
+        }
+    }
+}
