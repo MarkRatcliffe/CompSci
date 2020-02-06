2da73c236164fe6066bb30eaab4691b092c31bc3
mbr@hereford.ac.uk <mbr@hereford.ac.uk>
2020-02-06 15:57:50 +0000

    The website demonstrates HTML, CSS, Javascript

---
+++ WebExercise/mbrjs.js
@@ -0,0 +1,18 @@
+﻿console.log('starting javascript')
+
+function ValidateEmail() {
+    var inputText = document.forms["email"]["emailText"].value;
+    
+    console.log('starting ValidateEmail using' + inputText)
+    var regEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (regEx.test(inputText) ) {
+        alert("Your email address has been accepted");
+        document.form1.emailText.focus();
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        document.form1.emailText.focus();
+        return false;
+    }
+}
